06c25e4a5de9c88ee14cc5f869c891ff81e4b90d
AkmRifai05 <akmrifay05@gmail.com>
2023-11-30 16:50:54 +0530

    c

--- assets/js/google-form.js
+++ assets/js/google-form.js
@@ -3,19 +3,21 @@
   
     function submitGoogleForm(form) {
       try {
+       
         var data = [].slice.call(form).map(function(control) {
           return 'value' in control && control.name ?
             control.name + '=' + (control.value === undefined ? '' : control.value) :
             '';
         }).join('&');
         var xhr = new XMLHttpRequest();
-  
+        
         xhr.open('POST', form.action + '/formResponse', true);
         xhr.setRequestHeader('Accept',
             'application/xml, text/xml, */*; q=0.01');
         xhr.setRequestHeader('Content-type',
             'application/x-www-form-urlencoded; charset=UTF-8');
         xhr.send(data);
+        
       } catch(e) {}
   
       form.parentNode.className += ' submitted';
@@ -23,7 +25,7 @@
       var res = confirm("Thankyou for your response");
       if (res == true)
       {
-        window.location.replace("https://www.emd-labs.com/");
+        window.location.replace("https://www.spmlk.tech/");
       }
   
       return false;
